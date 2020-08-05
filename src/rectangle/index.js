b356e3004d357ef24cb20ac2ba0dada0e937c6f5
josh <josh-moss@live.nmit.ac.nz>
2020-08-06 11:53:04 +1200

    added code to retangle index.js, notes added.

---
+++ src/rectangle/index.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(710, 400);
+// createCanvas() allows us to create a canvas at a specific size.
+    background(255);
+// background() gives the canvas a background colour in this case 255 is white.
+    stroke(255, 0, 0);
+// stroke allows us to add a stroke and set its colour. In this case it is red.
+    strokeWeight(6);
+// strokeWeight() edits the thickness of the stroke. 
+}
+
+function draw() {
+// function draw allows us to create our shapes.
+    background(255);
+// background() allows us to set the shapes background. In this case it is white.
+    rect(30, 20, 200, 100, 20);
+// rectagnle params, used to create a rounded edge rectangle.
+}
