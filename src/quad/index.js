38b1eb69880611c024d136848089d7b3cada3399
josh <josh-moss@live.nmit.ac.nz>
2020-08-06 11:06:50 +1200

    added setup and draw functions to quad folder. plus notes.

---
+++ src/quad/index.js
@@ -0,0 +1,11 @@
+// function setup allows us to set up our canvas.
+function setup() {
+// createCanvas allows us to setup our drawing board and give it a size.
+    createCanvas(710, 400);
+}
+
+function draw() {
+// function draw allows us to start rendering our content we want on our webpage.
+    background(175, 100, 220);
+// background allows us to set our desired colour for our canvas. In this case it is blue.
+}
